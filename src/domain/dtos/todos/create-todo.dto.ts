98152fb1ff36b8cfc3cc7cc4e66d9454f12c06d9
Mateo Tamayo <christianmateotamayo@gmail.com>
2025-05-23 16:51:57 +0200

    few changes

--- src/domain/dtos/todos/create-todo.dto.ts
+++ src/domain/dtos/todos/create-todo.dto.ts
@@ -11,7 +11,7 @@ export class CreateTodoDto {
         const { text } = props
 
         if (!text) return ['Text property is required', undefined];
-        
+
         return [undefined, new CreateTodoDto(text)];
 
     }
